191d1d9683eaf7e6513def0b76f8806f6538ec03
hule.wei <hule.wei@cyclone-robotics.com>
2022-04-28 19:20:00 +0800

    0428

--- torch_classification/cifar10/predict.py
+++ torch_classification/cifar10/predict.py
@@ -1,19 +1,21 @@
 import os
+import sys
+sys.path.append(os.getcwd())
 import torch
 import cv2
 from torchvision import transforms, datasets
 from torchvision.utils import make_grid
-from model import Net
 import matplotlib.pyplot as plt
+from model import Net
 import numpy as np
 from torch.utils.data import DataLoader
 from torch.nn.functional import softmax
 
 def imshow(img):
     # 输入数据: torch.tensor[c, h, w]
-    img = img * 0.5 + 0.5
+    # img = img * 0.5 + 0.5
     img = np.transpose(img.cpu().numpy(), (1, 2, 0))
-    print(img.shape)
+    print(img)
     # cv2.imwrite("test.png", img)
 
 def main():
@@ -23,24 +25,29 @@ def main():
                                     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
                                         ])
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    # weight_path = "/workshop/cyclone_data/process/data/weights/cifar10/6499.pth"
+    # root = "/workshop/cyclone_data/process/data/DL"
+    weight_path = "/nfs/home57/weihule/data/weights/cifar10/6499.pth"
+    root = "/nfs/home57/weihule/data/DL"
     net = Net()
     net = net.to(device)
-    net.load_state_dict(torch.load("/workshop/cyclone_data/process/data/weights/cifar10/6499.pth"))
+    net.load_state_dict(torch.load(weight_path))
 
-    root = "/workshop/cyclone_data/process/data/DL"
-    testset = datasets.CIFAR10(root=root, train=False, transform=transform, download=True)
+    # testset = datasets.CIFAR10(root=root, train=False, transform=transform, download=True)
+    testset = datasets.CIFAR10(root=root, train=False, download=True)
     test_loader = DataLoader(testset, batch_size=MINI_BATCH, shuffle=False, num_workers=4)
 
     with torch.no_grad():
         for images, labels in test_loader:
             images, labels = images.to(device), labels.to(device)
-            img = make_grid(images)
-            imshow(img)
-            outputs = net(images)
-            probs = softmax(outputs, dim=1)
-            _, pred_y = torch.max(probs, dim=1)
-            print(pred_y.cpu().numpy(), pred_y.shape)
-            print(labels, labels.shape)
+            print(images)
+            # img = make_grid(images)
+            # imshow(img)
+            # outputs = net(images)
+            # probs = softmax(outputs, dim=1)
+            # _, pred_y = torch.max(probs, dim=1)
+            # print(pred_y.cpu().numpy(), pred_y.shape)
+            # print(labels, labels.shape)
 
             break
 
