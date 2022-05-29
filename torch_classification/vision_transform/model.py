10f2a89b3a03516613e523b6f4e04d47974d8294
weihl <weihule@163.com>
2022-05-30 07:54:26 +0800

    提交vit的模型部分, 还未完成

--- torch_classification/vision_transform/model.py
+++ torch_classification/vision_transform/model.py
@@ -1,22 +1,281 @@
 import torch
 import torch.nn as nn
+import numpy as np
 from collections import OrderedDict
 from functools import partial
+import torch.nn.functional as F
+
+
+def drop_path(x, drop_prob: float = 0., training: bool = False):
+    """
+    Drop paths (Stochastic Depth) per sample (when applied in main path of residual blocks).
+    This is the same as the DropConnect impl I created for EfficientNet, etc networks, however,
+    the original name is misleading as 'Drop Connect' is a different form of dropout in a separate paper...
+    See discussion: https://github.com/tensorflow/tpu/issues/494#issuecomment-532968956 ... I've opted for
+    changing the layer and argument names to 'drop path' rather than mix DropConnect as a layer name and use
+    'survival rate' as the argument.
+    """
+    if drop_prob == 0. or not training:
+        return x
+    keep_prob = 1 - drop_prob
+    shape = (x.shape[0],) + (1,) * (x.ndim - 1)  # work with diff dim tensors, not just 2D ConvNets
+    random_tensor = keep_prob + torch.rand(shape, dtype=x.dtype, device=x.device)
+    random_tensor.floor_()  # binarize
+    output = x.div(keep_prob) * random_tensor
+    return output
+
+
+class DropPath(nn.Module):
+    """
+    Drop paths (Stochastic Depth) per sample  (when applied in main path of residual blocks).
+    """
+
+    def __init__(self, drop_prob=None):
+        super(DropPath, self).__init__()
+        self.drop_prob = drop_prob
+
+    def forward(self, x):
+        return drop_path(x, self.drop_prob, self.training)
+
 
 class PatchEmbed(nn.Module):
     """
     2D Image to patch Embedding
     """
+
     def __init__(self, img_size=224, patch_size=16, in_c=3, embed_dim=768, norm_layer=None):
         super(PatchEmbed, self).__init__()
         self.img_size = (img_size, img_size)
         self.patch_size = (patch_size, patch_size)
-        self.grid_size = (img_size[0] // patch_size[0], img_size[1] // patch_size[1])
+        self.grid_size = (self.img_size[0] // self.patch_size[0], self.img_size[1] // self.patch_size[1])
         self.num_patches = self.grid_size[0] * self.grid_size[1]
 
-        self.proj = nn.Conv2d(in_channels=in_c, out_channels=embed_dim,     # 224*224*3 -> 14*14*768
-                            kernel_size=patch_size, stride=patch_size)
+        self.proj = nn.Conv2d(in_channels=in_c, out_channels=embed_dim,  # 224*224*3 -> 14*14*768
+                              kernel_size=patch_size, stride=patch_size)
         self.norm = norm_layer(embed_dim) if norm_layer else nn.Identity()
 
+    def forward(self, x):
+        B, C, H, W = x.shape
+        assert H == self.img_size[0] and W == self.img_size[0], \
+            "Input image size ({}*{}) doesn't match model".format(H, W)
+
+        # flatten: [B,C,H,W] -> [B,C,H*W]
+        # transpose: [B,C,H*W] -> [B,H*W,C]
+        x = self.proj(x).flatten(2).transpose(1, 2)  # [B,3,224,224] -> [B,14*14,768]
+        x = self.norm(x)
+
+
+class Attention(nn.Module):
+    def __init__(self,
+                 dim,  # 输入token的dim
+                 num_heads=8,
+                 qkv_bias=False,
+                 qk_scale=None,
+                 attn_drop_ratio=0.,
+                 proj_drop_ratio=0.):
+        super(Attention, self).__init__()
+        self.num_heads = num_heads
+        head_dim = dim // self.num_heads
+        self.scale = qk_scale or head_dim ** -0.5
+        self.qkv = nn.Linear(dim, dim * 3, bias=qkv_bias)  # 通过一个全连接层得到qkv
+        self.attn_drop = nn.Dropout(attn_drop_ratio)
+        self.proj = nn.Linear(dim, dim)
+        self.proj_drop = nn.Dropout(proj_drop_ratio)
+
+    def forward(self, x):
+        # [batch_size, num_patches+1, total_embed_dim]
+        B, N, C = x.shape
+
+        # self.qkv() -> [batch_size, num_patches+1, total_embed_dim*3]
+        # reshape: -> [batch_size, num_patches+1, 3, num_heads, embed_dim_per_head]
+        # permute: -> [3, batch_size, num_heads, num_patches+1, embed_dim_per_head]
+        qkv = self.qkv(x).reshape(B, N, 3, self.num_heads, C // self.num_heads).permute(2, 0, 3, 1, 4)
+
+        # 分成3个, 分别代表q, k, v
+        # [batch_size, num_heads, num_patches+1, embed_dim_per_head]
+        q, k, v = qkv[0], qkv[1], qkv[2]
+
+        # transpose: -> [batch_size, num_heads, embed_dim_per_head, num_patches+1]
+        # @: -> [batch_size, num_heads, num_patches+1, num_patches+1]
+        attn = (q @ k.tranpose(-2, -1)) * self.scale
+        attn = F.softmax(attn, dim=-1)
+        attn = self.attn_drop(attn)
+
+        # @: -> [batch_size, num_heads, num_patches+1, embed_dim_per_head]
+        # transpose: [batch_size, num_patches+1, num_heads, embed_dim_per_head]
+        # reshape: [batch_size, num_patches+1, total_embed_dim]
+        x = (attn @ v).transpose(1, 2).reshape(B, N, C)
+        x = self.proj(x)
+        x = self.proj_drop(x)
+        return x
+
+
+class Mlp(nn.Module):
+    def __init__(self, in_features, hidden_features=None,
+                 out_features=None, act_layer=nn.GELU, drop=0.):
+        super(Mlp, self).__init__()
+        # 如果有传入值，就用传入值，否则就是 in_features
+        out_features = out_features or in_features
+        hidden_features = hidden_features or in_features
+        self.fc1 = nn.Linear(in_features, hidden_features)
+        self.act = act_layer()
+        self.fc2 = nn.Linear(hidden_features, out_features)
+        self.drop = nn.Dropout(drop)
+
+    def forward(self, x):
+        x = self.fc1(x)
+        x = self.act(x)
+        x = self.drop(x)
+        x = self.fc2(x)
+        x = self.drop(x)
+        return x
+
+
+class Block(nn.Module):
+    def __init__(self,
+                 dim,
+                 num_heads,
+                 mlp_ratio=4.,
+                 qkv_bias=False,
+                 qk_scale=None,
+                 drop_ratio=0.,  # 对应multi-head attn中最后一个全连接层使用的dropout
+                 attn_drop_ratio=0.,  # 对应 Q*K.T*qk_scale**-0.5 经过softmax之后的dropout
+                 drop_path_ratio=0.,  # 对应block中 Mutil-head attn 和 MLP 之后的DropPath
+                 act_layer=nn.GELU,
+                 norm_layer=nn.LayerNorm):
+        super(Block, self).__init__()
+        self.norm1 = norm_layer(dim)
+        self.attn = Attention(dim=dim, num_heads=num_heads,
+                              qkv_bias=qkv_bias,
+                              qk_scale=qk_scale,
+                              attn_drop_ratio=attn_drop_ratio,
+                              proj_drop_ratio=drop_ratio)
+        # NOTE: drop path for stochastic depth, we shall see if this is better than dropout here 
+        self.drop_path = DropPath(drop_path_ratio) if drop_path_ratio > 0 else nn.Identity()
+        self.norm2 = norm_layer(dim)
+        self.mlp = Mlp(in_features=dim,
+                       out_features=int(dim * mlp_ratio),
+                       act_layer=act_layer,
+                       drop=drop_path_ratio
+                       )
+
+    def forward(self, x):
+        x = x + self.drop_path(self.attn(self.norm1(x)))
+        x = x + self.drop_path(self.mlp(self.norm2(x)))
+        return x
+
+
+class VisionTransformer(nn.Module):
+    def __init__(self, img_size=224, patch_size=16, in_c=3, num_classes=1000,
+                 embed_dim=768, depth=12, num_heads=12, mlp_ratio=4, qkv_bias=True,
+                 qk_scale=None, representation_size=None, distilled=False,
+                 drop_ratio=0., attn_drop_ratio=0., drop_path_ratio=0.,
+                 embed_layer=PatchEmbed, norm_layer=None, act_layer=None):
+        """
+        Args:
+            img_size (int, tuple): input image size
+            patch_size (int, tuple): patch size
+            in_c (int): number of input channels
+            num_classes (int): number of classes for classification head
+            embed_dim (int): embedding dimension
+            depth (int): depth of transformer
+            num_heads (int): number of attention heads
+            mlp_ratio (int): ratio of mlp hidden dim to embedding dim
+            qkv_bias (bool): enable bias for qkv if True
+            qk_scale (float): override default qk scale of head_dim ** -0.5 if set
+            representation_size (Optional[int]): enable and set representation layer (pre-logits) to this value if set
+            distilled (bool): model includes a distillation token and head as in DeiT models
+            drop_ratio (float): dropout rate
+            attn_drop_ratio (float): attention dropout rate
+            drop_path_ratio (float): stochastic depth rate
+            embed_layer (nn.Module): patch embedding layer
+            norm_layer: (nn.Module): normalization layer
+        """
+        super(VisionTransformer, self).__init__()
+        self.num_classes = num_classes
+        self.num_features = self.embed_dim = embed_dim
+        self.num_tokens = 2 if distilled else 1  # 这里distilled这个参数为False
+        norm_layer = norm_layer or partial(nn.LayerNorm, eps=1e-6)
+        act_layer = act_layer or nn.GELU
+
+        self.patch_embed = embed_layer(img_size=img_size, patch_size=patch_size, in_c=in_c, embed_dim=embed_dim)
+        num_patches = self.patch_embed.num_patches
+
+        self.cls_token = nn.Parameter(torch.zeros(1, 1, embed_dim))  # [1, 1, embed_dim] 第一个1代表batch维度
+        self.dist_token = nn.Parameter(torch.zeros(1, 1, embed_dim)) if distilled else None
+        self.pos_embed = nn.Parameter(
+            torch.zeros(1, num_patches + self.num_tokens, embed_dim))  # [196,768]和[1,768]cat之后, 加pos_embed
+        self.pos_drop = nn.Dropout(drop_ratio)
+
+        drp = [x.item() for x in torch.linspace(0, drop_path_ratio, depth)]
+        self.blocks = nn.Sequential(*[
+            Block(dim=embed_dim, num_heads=num_heads, mlp_ratio=mlp_ratio,
+                  qkv_bias=qkv_bias, qk_scale=qk_scale, drop_ratio=drp[i],
+                  attn_drop_ratio=attn_drop_ratio, drop_path_ratio=drop_path_ratio,
+                  act_layer=act_layer, norm_layer=norm_layer)
+            for i in range(depth)
+        ])
+        self.norm = norm_layer(embed_dim)
+
+        # Representation layer
+        if representation_size:
+            pass
+        else:
+            self.has_logits = False
+            self.pre_logits = nn.Identity()
+
+        # Classifier heads
+        self.head = nn.Linear(self.num_features, num_classes) if num_classes > 0 else nn.Identity()
+        self.head_dist = None
+
+        # Weight init
+        nn.init.trunc_normal_(self.pos_embed, std=0.02)  # 截断正态分布
+        if self.dist_token is not None:
+            nn.init.trunc_normal_(self.dist_token, std=0.02)
+
+        nn.init.trunc_normal_(self.cls_token, std=0.02)
+        self.apply(_init_vit_weights)
+
+    def forward_features(self, x):
+        # [B, C, H, W] -> [B, num_pathes, embed_dim]
+        x = self.patch_embed(x)  # [B, 196, 768]
+        # [1, 1, 768] -> [B, 1, 768]
+        cls_token = self.cls_token.expand(x.shape[0], -1, -1)  # -1表示该维度不变
+        if self.dist_token is None:
+            x = torch.cat((cls_token, x), dim=1)
+
+        x = self.pos_drop(x + self.pos_embed)
+        x = self.blocks(x)
+        x = self.norm(x)
+        if self.dist_token is None:
+            return self.pre_logits(x[:, 0])  # 这里self.pre_logits是 nn.Identity(), 第一维是batch,所以全都要
+        else:
+            return x[:, 0], x[:, 1]
+
+    def forward(self, x):
+        x = self.forward_features(x)
+        return self.head(x)
+
+
+def _init_vit_weights(m):
+    """
+    ViT weight initialization
+    :param m: module
+    """
+    if isinstance(m, nn.Linear):
+        nn.init.trunc_normal_(m.weight, std=0.01)
+        if m.bias is not None:
+            nn.init.zeros_(m.bias)
+    elif isinstance(m, nn.Conv2d):
+        nn.init.kaiming_normal_(m.weight, mode='fan_out')
+        if m.bias is not None:
+            nn.init.zeros_(m.bias)
+    elif isinstance(m, nn.LayerNorm):
+        nn.init.ones_(m.weight)
+        nn.init.zeros_(m.weight)
+
+
 if __name__ == "__main__":
-    pass
+    a = torch.tensor([[1, 2], [3, 4], [5, 6]])      # 3, 2
+    c = torch.repeat_interleave(a, repeats=2, dim=1)
+    print(c, a.shape)
---
+++ torch_classification/vision_transform/test.py
@@ -0,0 +1,7 @@
+import torch
+import torch.nn as nn
+
+# hyper parameters
+in_dim = 1
+n_hidden_1 = 2
+n_hidden_1 = 2
