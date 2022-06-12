c4562120709fd70846d3d38480076ff4457975bc
weihl <weihule@163.com>
2022-06-13 07:35:58 +0800

    增加resnet训练cifar10部分

--- torch_classification/ResNet/model.py
+++ torch_classification/ResNet/model.py
@@ -1,21 +1,47 @@
 import torch
 import torch.nn as nn
+from collections import OrderedDict
+from torch import Tensor, FloatTensor
+
+
+class SELayer(nn.Module):
+    def __init__(self, in_channel, reduction=16):
+        super(SELayer, self).__init__()
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.fc = nn.Sequential(
+            nn.Linear(in_channel, in_channel // reduction, bias=True),
+            nn.ReLU(True),
+            nn.Linear(in_channel // reduction, in_channel, bias=True),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x: Tensor):
+        b, c, _, _ = x.shape
+        y = self.avg_pool(x)
+        y = torch.flatten(y, start_dim=1)
+        y = self.fc(y).view(b, c, 1, 1)
+
+        return x * y.expand_as(x)
+
 
 class BasicBlock(nn.Module):
     expansion = 1
 
-    def __init__(self, in_channel, out_channel, stride=1, downsample=None, **kwargs):
+    def __init__(self, in_channel, out_channel, stride=1, downsample=None, use_se=False, **kwargs):
         super(BasicBlock, self).__init__()
         self.conv1 = nn.Conv2d(in_channel, out_channel,
-                                kernel_size=3, stride=stride,
-                                padding=1, bias=False)
+                               kernel_size=3, stride=stride,
+                               padding=1, bias=False)
         self.bn1 = nn.BatchNorm2d(out_channel)
         self.relu = nn.ReLU(inplace=True)
         self.conv2 = nn.Conv2d(out_channel, out_channel,
-                                kernel_size=3, stride=1,
-                                padding=1, bias=False)
+                               kernel_size=3, stride=1,
+                               padding=1, bias=False)
         self.bn2 = nn.BatchNorm2d(out_channel)
         self.downsample = downsample
+        self.use_se = use_se
+        if self.use_se:
+            self.se = SELayer(out_channel)
 
     def forward(self, x):
         identity = x
@@ -27,12 +53,15 @@ class BasicBlock(nn.Module):
         out = self.relu(out)
 
         out = self.conv2(out)
-        out = self.bn2(out) 
+        out = self.bn2(out)
 
         out += identity
         out = self.relu(out)
-    
-        return out 
+        if self.use_se:
+            out = self.se(out)
+
+        return out
+
 
 class Bottleneck(nn.Module):
     """
@@ -44,22 +73,22 @@ class Bottleneck(nn.Module):
     expansion = 4
 
     def __init__(self, in_channel, out_channel, stride=1, downsample=None,
-                groups=1, width_per_group=64):
+                 groups=1, width_per_group=64):
         super(Bottleneck, self).__init__()
 
         # # default : width = out_channel
         # width = int(out_channel * (width_per_group / 64.)) * groups
 
         self.conv1 = nn.Conv2d(in_channels=in_channel, out_channels=out_channel,
-                                kernel_size=1, stride=1, bias=False)
+                               kernel_size=1, stride=1, bias=False)
         self.bn1 = nn.BatchNorm2d(out_channel)
         # -----------------------------------------
         self.conv2 = nn.Conv2d(in_channels=out_channel, out_channels=out_channel,
-                                kernel_size=3, stride=stride, padding=1, bias=False)
+                               kernel_size=3, stride=stride, padding=1, bias=False)
         self.bn2 = nn.BatchNorm2d(out_channel)
         # -----------------------------------------
         self.conv3 = nn.Conv2d(in_channels=out_channel, out_channels=out_channel * self.expansion,
-                                kernel_size=1, stride=1, bias=False)
+                               kernel_size=1, stride=1, bias=False)
         self.bn3 = nn.BatchNorm2d(out_channel * self.expansion)
         self.relu = nn.ReLU(inplace=True)
         self.downsample = downsample
@@ -85,14 +114,15 @@ class Bottleneck(nn.Module):
 
         return out
 
+
 class ResNet(nn.Module):
     def __init__(self,
-                block, 
-                blocks_num,
-                num_classes=1000,
-                include_top=True,
-                groups=1,
-                width_per_group=64):
+                 block,
+                 blocks_num,
+                 num_classes=1000,
+                 include_top=True,
+                 groups=1,
+                 width_per_group=64):
         super(ResNet, self).__init__()
         self.include_top = include_top
         self.in_channel = 64
@@ -101,19 +131,20 @@ class ResNet(nn.Module):
         self.width_per_group = width_per_group
 
         self.conv1 = nn.Conv2d(in_channels=3, out_channels=self.in_channel,
-                                kernel_size=7, stride=2, padding=3, bias=False)
+                               kernel_size=7, stride=2, padding=3, bias=False)
         self.bn1 = nn.BatchNorm2d(self.in_channel)
+        self.relu = nn.ReLU(inplace=True)
 
         self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
         self.layer1 = self._make_layer(block, 64, blocks_num[0], stride=1)
         self.layer2 = self._make_layer(block, 128, blocks_num[1], stride=2)
         self.layer3 = self._make_layer(block, 256, blocks_num[2], stride=2)
         self.layer4 = self._make_layer(block, 512, blocks_num[3], stride=2)
-        self.relu = nn.ReLU(inplace=True)
+
         if self.include_top:
-             self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
-             self.fc = nn.Linear(512 * block.expansion, num_classes)
-        
+            self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+            self.fc = nn.Linear(512 * block.expansion, num_classes)
+
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
                 nn.init.kaiming_normal_(m.weight, mode="fan_out", nonlinearity="relu")
@@ -130,7 +161,8 @@ class ResNet(nn.Module):
        对于layer2, 3, 4来说, stride == 2时, 即第一层需要进行下采样
         对于layer1来说, 所有层都不需要进行下采样, 但是其第一层
         需要进行卷积操作, stride == 1, 但是self.in_channel == 64, 
-        但是 channel * block.expansion == 256, 这两者不相等
+        但是 channel * block.expansion == 256, 这两者不相等，但是
+        仍然有shortcut连接, 所以仍然需要右边进行一个卷积操作来调整channel
 
         另外, 这里的stride是为了layer的第一层设置的, 传到了block里
         其余层的stride都是固定的, 已经在block中设置了
@@ -138,7 +170,7 @@ class ResNet(nn.Module):
         if stride != 1 or self.in_channel != channel * block.expansion:
             downsample = nn.Sequential(
                 nn.Conv2d(self.in_channel, channel * block.expansion,
-                        kernel_size=1, stride=stride, bias=False),
+                          kernel_size=1, stride=stride, bias=False),
                 nn.BatchNorm2d(channel * block.expansion)
             )
         layers = []
@@ -149,9 +181,9 @@ class ResNet(nn.Module):
                             stride=stride,
                             downsample=downsample,
                             groups=self.groups,
-                            width_per_group=self.width_per_group
+                            width_per_group=self.width_per_group,
                             )
-                    )
+                      )
 
         # 每个layer的第一层之后, self.in_channel就变成channel的倍数了
         self.in_channel = channel * block.expansion
@@ -162,7 +194,7 @@ class ResNet(nn.Module):
                                 groups=self.groups,
                                 width_per_group=self.width_per_group
 
-            ))
+                                ))
         return nn.Sequential(*layers)
 
     def forward(self, x):
@@ -183,9 +215,94 @@ class ResNet(nn.Module):
 
         return x
 
+
+class ResNetCIFAR(nn.Module):
+    """
+    为CIFAR10 (32*32) 的数据集做了改造
+    """
+
+    def __init__(self,
+                 block,
+                 blocks_num: list,
+                 num_classes: int = 1000,
+                 include_top: bool = True,
+                 groups: int = 1,
+                 width_per_group: int = 64):
+        super(ResNetCIFAR, self).__init__()
+        self.include_top = include_top
+        self.in_channel = 64
+        self.groups = groups
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=self.in_channel,
+                               kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(self.in_channel)
+        self.relu = nn.ReLU(inplace=True)
+        # 这里改变刚开始的 conv1，抛弃 Maxpool 层，直接开始各个 residual block
+        self.layer1 = self._make_layer_cifar(block, 64, blocks_num[0], stride=1)  # 32*32, 64
+        self.layer2 = self._make_layer_cifar(block, 128, blocks_num[1], stride=2)  # 16*16, 128
+        self.layer3 = self._make_layer_cifar(block, 256, blocks_num[2], stride=2)  # 8*8, 256
+        self.layer4 = self._make_layer_cifar(block, 512, blocks_num[3], stride=2)  # 4*4, 512
+
+        if self.include_top:
+            self.avg_pool = nn.AdaptiveAvgPool2d(1)
+            self.fc = nn.Linear(512 * block.expansion, num_classes)
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode="fan_out", nonlinearity="relu")
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.Linear):
+                nn.init.xavier_normal_(m.weight)
+                nn.init.constant_(m.bias, 0)
+
+    def _make_layer_cifar(self, block, channel, block_num, stride=1):
+        downsample = None
+        if stride != 1 or self.in_channel != channel * block.expansion:
+            downsample = nn.Sequential(
+                nn.Conv2d(self.in_channel, channel * block.expansion,
+                          kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(channel * block.expansion)
+            )
+        layers = OrderedDict()
+        layers.update({'block1': block(self.in_channel, channel, stride=stride, downsample=downsample, use_se=True)})
+        self.in_channel = channel * block.expansion
+
+        for i in range(block_num - 1):
+            key = 'block' + str(i + 2)
+            layers.update({key: block(self.in_channel, channel, stride=1, use_se=True)})
+
+        return nn.Sequential(layers)
+
+    def forward(self, x: Tensor):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+
+        if self.include_top:
+            x = self.avg_pool(x)
+            x = torch.flatten(x, start_dim=1)
+            x = self.fc(x)
+
+        return x
+
+
 def resnet50(num_classes=1000, include_top=True):
     return ResNet(Bottleneck, [3, 4, 6, 3], num_classes=num_classes, include_top=include_top)
 
-# if __name__ == "__main__":
-#     model = resnet50(num_classes=5)
-#     print(model)
+
+def resnet18(num_classes=1000, include_top=True):
+    return ResNetCIFAR(BasicBlock, [2, 2, 2, 2], num_classes=num_classes, include_top=include_top)
+
+
+if __name__ == "__main__":
+    model = resnet18(num_classes=5)
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    model = model.to(device)
+    input_data = torch.randn(size=(64, 3, 32, 32))
+    pred = model(input_data)
+    print(pred.shape)
--- torch_classification/ResNet/train.py
+++ torch_classification/ResNet/train.py
@@ -1,83 +1,88 @@
 import os
+import sys
 import torch
 import torch.nn as nn
 from torchvision import transforms, datasets
 from torch.utils.data import DataLoader
+from torch.optim import Adam, lr_scheduler
+import torchvision
 from tqdm import tqdm
 import json
 import glob
 import random
-from mydataset import MyDataSet
+from custom_dataset import CustomDataset
 import matplotlib.pyplot as plt
 from PIL import Image
 import cv2
+import math
+from model import resnet50, resnet18
 
-from model import resnet50
-# from torchvision.models import resnet50
+sys.path.append(os.getcwd())
+
 
 def main():
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    print("using {} device".format(device) )
-    
+    print("using {} device".format(device))
+
     data_transform = {
         "train": transforms.Compose([transforms.RandomResizedCrop(224),
-                transforms.RandomHorizontalFlip(),
-                transforms.ToTensor(),
-                transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])]),
+                                     transforms.RandomHorizontalFlip(),
+                                     transforms.ToTensor(),
+                                     transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])]),
         "val": transforms.Compose([transforms.RandomResizedCrop(224),
-                transforms.ToTensor(),
-                transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])]),
+                                   transforms.ToTensor(),
+                                   transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])]),
     }
 
     root = "../dataset"
 
     with open("./demo/class_indices.json") as f:
         cls_idx = json.load(f)
-    reverse_idx = {v:k for k, v in cls_idx.items()}
-    
-#     # train_images
-#     train_img_path = []
-#     train_img_label = []
+    reverse_idx = {v: k for k, v in cls_idx.items()}
 
-#     for i in glob.glob(os.path.join(root, "train/*/*")):
-#         train_img_path.append(i)
-    
-#     random.shuffle(train_img_path)
-#     # print(train_img_path[0])
-#     for i in train_img_path:
-#         train_img_label.append(int(reverse_idx[i.split(os.sep)[-2]]))
-    
-#    # val_images
-#     val_img_path = []
-#     val_img_label = []
+    #     # train_images
+    #     train_img_path = []
+    #     train_img_label = []
 
-#     for i in glob.glob(os.path.join(root, "val/*/*")):
-#         val_img_path.append(i)
-    
-#     random.shuffle(val_img_path)
-#     for i in val_img_path:
-#         val_img_label.append(int(reverse_idx[i.split(os.sep)[-2]]))
+    #     for i in glob.glob(os.path.join(root, "train/*/*")):
+    #         train_img_path.append(i)
+
+    #     random.shuffle(train_img_path)
+    #     # print(train_img_path[0])
+    #     for i in train_img_path:
+    #         train_img_label.append(int(reverse_idx[i.split(os.sep)[-2]]))
+
+    #    # val_images
+    #     val_img_path = []
+    #     val_img_label = []
+
+    #     for i in glob.glob(os.path.join(root, "val/*/*")):
+    #         val_img_path.append(i)
+
+    #     random.shuffle(val_img_path)
+    #     for i in val_img_path:
+    #         val_img_label.append(int(reverse_idx[i.split(os.sep)[-2]]))
 
     BATCHSIZE = 4
     nw = 4
     # ------------------------------------------------------------
     # train_ds = MyDataSet(train_img_path, train_img_label, data_transform["train"])
-    
+
     train_ds = datasets.ImageFolder(root=os.path.join(root, "train"), transform=data_transform["train"])
     train_loader = DataLoader(train_ds,
-                    batch_size=BATCHSIZE,
-                    shuffle=True,
-                    num_workers=nw,
-                    )
+                              batch_size=BATCHSIZE,
+                              shuffle=True,
+                              num_workers=nw,
+                              )
     train_num = len(train_ds)
 
     # validate_ds = MyDataSet(val_img_path, val_img_label, data_transform["train"])
-    
+
     validate_ds = datasets.ImageFolder(root=os.path.join(root, "val"), transform=data_transform["val"])
     validate_loader = DataLoader(validate_ds,
-                    batch_size=BATCHSIZE,
-                    shuffle=False,
-                    num_workers=nw)
+                                 batch_size=BATCHSIZE,
+                                 shuffle=False,
+                                 num_workers=nw)
     val_num = len(validate_ds)
 
     print("using {} imgaes for training, {} images for validation".format(train_num, val_num))
@@ -87,13 +92,13 @@ def main():
         images, labels = next(iter(train_loader))
         print(images.size(), labels.size())
         for i in range(BATCHSIZE):
-            plt.subplot(1, BATCHSIZE, i+1)
+            plt.subplot(1, BATCHSIZE, i + 1)
             index = str(labels[i].item())
             plt.title(index)
             mean = torch.tensor((0.485, 0.456, 0.406))
             std = torch.tensor((0.229, 0.224, 0.225))
             mean = mean.view(3, 1, 1).expand(3, 224, 224)
-            std  = std.view(3, 1, 1).expand(3, 224, 224)
+            std = std.view(3, 1, 1).expand(3, 224, 224)
             images[i] = images[i] * std + mean
             plt.imshow(images[i].permute(1, 2, 0))
             plt.axis("off")
@@ -162,7 +167,92 @@ def main():
             torch.save(net.state_dict(), save_path)
 
     print("Finished Training")
+
+
+def train_cifar():
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    batch_size = 128
+    epochs = 100
+    lr = 0.001
+    lrf = 0.001
+    data_transform = {
+        'train': transforms.Compose([transforms.RandomCrop(32, padding=4),
+                                     transforms.RandomHorizontalFlip(),
+                                     transforms.ToTensor(),
+                                     transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
+                                     ]),
+
+        'val': transforms.Compose([transforms.ToTensor(),
+                                   transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))])
+    }
+
+    # data_path = 'D:\\workspace\\data\\DL'
+    data_path = '/nfs/home57/weihule/data/DL'
+    # data_path = '/workshop/weihule/data/DL'
+
+    log_file = '/nfs/home57/weihule/data/weights/resnet/resnet18_cifar.txt'
+    train_set = torchvision.datasets.CIFAR10(root=data_path,
+                                             train=True,
+                                             transform=data_transform['train'],
+                                             download=True)
+    val_set = torchvision.datasets.CIFAR10(root=data_path,
+                                           train=False,
+                                           transform=data_transform['val'],
+                                           download=True)
+    print('the number of training: {}'.format(len(train_set)))
+    print('the number of validation: {}'.format(len(val_set)))
+
+    train_loader = DataLoader(train_set, batch_size=batch_size, shuffle=True, num_workers=4)
+    val_loader = DataLoader(val_set, batch_size=batch_size, shuffle=False, num_workers=4)
+
+    model = resnet18(num_classes=10)
+    model = model.to(device)
+
+    optimizer = Adam(model.parameters(), lr=lr)
+    lf = lambda x: ((1 + math.cos(x * math.pi / epochs)) / 2) * (1 - lrf) + lrf  # cosine
+    scheduler = lr_scheduler.LambdaLR(optimizer, lr_lambda=lf)
+
+    loss_function = nn.CrossEntropyLoss()
+    best_acc = 0.
+    for epoch in range(epochs):
+        model.train()
+        mean_loss = 0.
+        train_bar = tqdm(train_loader)
+        for datas in train_bar:
+            images, labels = datas
+            images, labels = images.to(device), labels.to(device)
+            optimizer.zero_grad()
+            pred_y = model(images)
+            loss = loss_function(pred_y, labels)
+            loss.backward()
+            mean_loss += loss.item()
+            optimizer.step()
+
+            train_bar.desc = '[epoch {}] loss {}'.format(epoch + 1, round(loss.item(), 3))
+
+        print('mean_loss: {}'.format(round(mean_loss / (len(train_loader) + 1), 3)))
+
+        # 每个epoch之后, 更新lr
+        scheduler.step()
+
+        # val
+        model.eval()
+        val_bar = tqdm(val_loader)
+        correct = 0
+        all_num = len(val_set)
+        for images, labels in val_bar:
+            images, labels = images.to(device), labels.to(device)
+            pred = model(images)
+            pred = torch.argmax(pred, dim=1)
+            correct += torch.eq(pred, labels).sum().item()
+
+        print('[epoch {}] accuracy {} ({}/{})'.format(epoch + 1, round(correct / all_num, 3), correct, all_num))
+
+        with open(log_file, 'a', encoding='utf-8') as f:
+            f.write(str(round(mean_loss / (len(train_loader) + 1), 3)) + '\t' + str(round(correct / all_num, 3)) + '\n')
+
+
 if __name__ == "__main__":
-    main()
+    # main()
 
-
+    train_cifar()
