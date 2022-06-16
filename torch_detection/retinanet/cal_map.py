54067a342d066662a8f43cc2375bdc8ba7005357
weihl <weihule@163.com>
2022-06-17 07:30:03 +0800

    222

--- torch_detection/retinanet/cal_map.py
+++ torch_detection/retinanet/cal_map.py
@@ -1,15 +1,28 @@
 import torch
 import numpy as np
+import time
 from shapely.geometry import Polygon
 
-arr1 = [1, 1, 4, 1, 4, 6, 1, 6]
-arr2 = [2, 3, 7, 3, 7, 9, 2, 9]
-area1 = Polygon(np.array(arr1).reshape(4, 2))
-area2 = Polygon(np.array(arr2).reshape(4, 2))
-inter = area1.intersection(area2).area
-print(area1, area2, inter)
 
-ar1 = [1, 1, 4, 6]
-ar1 = [1, 1, 4, 6]
+
+
 def iou(bbox1, bbox2):
-    pass
+    inter = (min(bbox1[2], bbox2[2]) - max(bbox1[0], bbox2[0])) * \
+            (min(bbox1[3], bbox2[3]) - max(bbox1[1], bbox2[1]))
+    print(inter)
+
+
+if __name__ == "__main__":
+    start = time.time()
+    # arr1 = [1, 1, 4, 1, 4, 6, 1, 6]
+    # arr2 = [2, 3, 7, 3, 7, 9, 2, 9]
+    # area1 = Polygon(np.array(arr1).reshape(4, 2))
+    # area2 = Polygon(np.array(arr2).reshape(4, 2))
+    # inter = area1.intersection(area2).area
+    # print(area1, area2, inter)
+
+    ar1 = [1, 1, 4, 6]
+    ar2 = [2, 3, 7, 9]
+    iou(ar1, ar2)
+    end = start - time.time()
+    print(f'running time: {end-start}')
