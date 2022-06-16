c4c7bf254f1980fc192a1682e7cc69662ecda2e5
weihl <weihule@163.com>
2022-06-17 00:03:31 +0800

    22

--- torch_detection/retinanet/cal_map.py
+++ torch_detection/retinanet/cal_map.py
@@ -3,6 +3,13 @@ import numpy as np
 from shapely.geometry import Polygon
 
 arr1 = [1, 1, 4, 1, 4, 6, 1, 6]
-area1 = Polygon(np.array(arr1).reshape(4, 2)).area
-print(area1)
+arr2 = [2, 3, 7, 3, 7, 9, 2, 9]
+area1 = Polygon(np.array(arr1).reshape(4, 2))
+area2 = Polygon(np.array(arr2).reshape(4, 2))
+inter = area1.intersection(area2).area
+print(area1, area2, inter)
 
+ar1 = [1, 1, 4, 6]
+ar1 = [1, 1, 4, 6]
+def iou(bbox1, bbox2):
+    pass
