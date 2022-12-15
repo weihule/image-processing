47e1709e1fdb67439fe8df4805a9ef4f8ae732c7
weihl <weihule@163.com>
2022-12-15 09:23:22 +0800

    add some content

--- py_other/py/process_excel.py
+++ py_other/py/process_excel.py
@@ -5,20 +5,12 @@ def main():
     wb = Workbook()
     ws = wb.active
 
-    ws1 = wb.create_sheet('sheet1', 1)
-    ws2 = wb.create_sheet('sheet2', 2)
+    for i in range(1, 11):
+        for j in range(1, 6):
+            ws.cell(i, j, i+j)
 
-    # 获取名为 ‘sheet2’ 的表
-    get_ws2 = wb['sheet2']
-    print(get_ws2.title)
-
-    wb.move_sheet(ws2, -1)
-
-    cp_sheet = wb.copy_worksheet(ws1)
-
-    print(wb.sheetnames)
-
-    wb.save(r'D:\Desktop\delete_excel\first.xlsx')
+    print(ws['a:c'])
+    wb.save(r'first.xlsx')
 
 
 if __name__ == "__main__":
