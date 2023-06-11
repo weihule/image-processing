c9ffa8667536d92613975902dbec6fe8f4482801
weihule <weihule@outlook.com>
2023-06-11 20:25:14 +0800

    add some content

--- py_other/py/process_excel.py
+++ py_other/py/process_excel.py
@@ -1,25 +1,25 @@
-# from openpyxl import Workbook, load_workbook
-# from openpyxl.utils import FORMULAE
+from openpyxl import Workbook, load_workbook
+from openpyxl.utils import FORMULAE
 import datetime
 import sys
 from pathlib import Path
 import cv2
 
 
-# def main():
-#     wb = Workbook()
-#     ws = wb.active
-#
-#     ws.append(['价格1', '价格2', '总和', '均值'])
-#
-#     ws.append([22, 63])
-#     ws.append([11, 88])
-#     ws.append([15, 68])
-#
-#     ws["c2"] = "=SUM(A2, B2)"   # 求和
-#     ws["d2"] = "=AVERAGE(A2: B2)"  # 求平均值
-#
-#     wb.save('test.xlsx')
+def main():
+    wb = Workbook()
+    ws = wb.active
+
+    ws.append(['价格1', '价格2', '总和', '均值'])
+
+    ws.append([22, 63])
+    ws.append([11, 88])
+    ws.append([15, 68])
+
+    ws["c2"] = "=SUM(A2, B2)"   # 求和
+    ws["d2"] = "=AVERAGE(A2: B2)"  # 求平均值
+
+    wb.save('test.xlsx')
 
 
 def test():
