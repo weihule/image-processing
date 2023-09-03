32727ed207e7950064853f199381338d591d85fe
weihule <weihule@outlook.com>
2023-09-03 21:50:10 +0800

    add some content

--- torch_classification/backbones/vit_model.py
+++ torch_classification/backbones/vit_model.py
@@ -4,13 +4,43 @@ from functools import partial
 from collections import OrderedDict
 
 
+def drop_path(x, drop_prob: float = 0., training: bool = False):
+    if drop_prob == 0. or not training:
+        return x
+    keep_prob = 1 - drop_prob
+    # work with diff dim tensors, not just 2D ConvNets
+    shape = (x.shape[0], ) + (1, )  * (x.ndim - 1)
+    random_tensor = keep_prob + torch.rand(shape, 
+                                           dtype=x.dtype,
+                                           device=x.device)
+    random_tensor.floor_()
+    output = x.div(keep_prob) * random_tensor
+
+    return output
+
+
+class DropPath(nn.Module):
+    """
+    Drop paths (Stochastic Depth) per sample (when applied in main path of residual blocks)
+    """
+    def __init__(self, drop_prob=None):
+        super(DropPath, self).__init__()
+        self.drop_prob = drop_prob
+    
+    def forward(self, x):
+        return drop_path(x, self.drop_prob, self.training)
+
+
 class PatchEmbed(nn.Module):
+    """
+    2D image to patch embedding
+    """
     def __init__(self, img_size=224, patch_size=16, in_c=3, embed_dim=768, norm_layer=None):
         super(PatchEmbed, self).__init__()
         img_size = (img_size, img_size)
         self.img_size = img_size
         patch_size = (patch_size, patch_size)
-        grid_size = (img_size[0] // patch_size[0], img_size[1] // img_size[1])
+        grid_size = (img_size[0] // patch_size[0], img_size[1] // patch_size[1])
         self.num_patches = grid_size[0] * grid_size[1]
 
         self.proj = nn.Conv2d(in_c, embed_dim, kernel_size=patch_size,
@@ -80,14 +110,75 @@ class Attention(nn.Module):
         return x
 
 
-class VisionTransform(nn.Module):
+class Mlp(nn.Module):
+    def __init__(self,
+                 in_features,
+                 hidden_features=None, 
+                 out_features=None,
+                 act_layer=nn.GELU(),
+                 drop=0.):
+        super(Mlp, self).__init__()
+        out_features = out_features or in_features
+        hidden_features = hidden_features or in_features
+        self.fc1 = nn.Linear(in_features, hidden_features)
+        self.act = act_layer
+        self.fc2 = nn.Linear(hidden_features, out_features)
+        self.drop = nn.Dropout(drop)
+    
+    def forward(self, x):
+        x = self.fc1(x)
+        x = self.act(x)
+        x = self.drop(x)
+        x = self.fc2(x)
+        x = self.drop(x)
+
+        return x
+    
+
+class Block(nn.Module):
+    def __init__(self,
+                 dim,
+                 num_heads,
+                 mlp_ratio=4.,
+                 qkv_bias=False,
+                 qk_scale=None,
+                 drop_ratio=0.,
+                 attn_drop_ratio=0.,
+                 drop_path_ratio=0.,
+                 act_layer=nn.GELU(),
+                 norm_layer=nn.LayerNorm):
+        super(Block, self).__init__()
+        self.norm1 = norm_layer(dim)
+        self.attn = Attention(dim, 
+                              num_heads=num_heads,
+                              qkv_bias=qkv_bias,
+                              qk_scale=qk_scale,
+                              attn_drop_ratio=attn_drop_ratio,
+                              proj_drop_ratio=drop_ratio)
+        # NOTE: drop path dor stochastic depth, we shall see if this better than dropout
+        self.drop_path = DropPath(drop_path_ratio) if drop_path_ratio > 0. else nn.Identity()
+        self.norm2 = norm_layer(dim)
+        mlp_hidden_dim = int(dim * mlp_ratio)
+        self.mlp = Mlp(in_features=dim,
+                       hidden_features=mlp_hidden_dim,
+                       act_layer=act_layer,
+                       drop=drop_ratio)
+    
+    def forward(self, x):
+        x = x + self.drop_path(self.attn(self.norm1(x)))
+        x = x + self.drop_path(self.mlp(self.norm2(x)))
+
+        return x
+
+
+class VisionTransformer(nn.Module):
     def __init__(self, img_size=224, patch_size=16, in_c=3, num_classes=1000,
                  embed_dim=768, depth=12, num_heads=12, mlp_ratio=4.0,
                  qkv_bias=True, qk_scale=None, representation_size=None,
                  distilled=False, drop_ratio=0., attn_drop_ratio=0.,
                  drop_path_ratio=0., embed_layer=PatchEmbed, norm_layer=None,
                  act_layer=None):
-        super(VisionTransform, self).__init__()
+        super(VisionTransformer, self).__init__()
         self.num_classes = num_classes
         self.num_features = self.embed_dim = embed_dim
         self.num_tokens = 2 if distilled else 1
@@ -101,6 +192,7 @@ class VisionTransform(nn.Module):
         # if input size is 224*224, num_patches = (224*224)/(16*16) = 196
         num_patches = self.patch_embed.num_patches
 
+        # TODO: self.cls_token 和 self.dist_token 是什么作用
         self.cls_token = nn.Parameter(torch.zeros(1, 1, embed_dim))
         self.dist_token = nn.Parameter(torch.zeros(1, 1, embed_dim)) if distilled else None
         # if input size is 224*224, pos_embed: [1, 196+1, 768]
@@ -109,32 +201,290 @@ class VisionTransform(nn.Module):
 
         # stochastic depth decay rule 随机深度衰减
         dpr = [x.item() for x in torch.linspace(0, drop_path_ratio, depth)]
+        self.blocks = nn.Sequential(*[
+            Block(dim=embed_dim, 
+                  num_heads=num_heads, 
+                  mlp_ratio=mlp_ratio, 
+                  qkv_bias=qkv_bias,
+                  qk_scale=qk_scale, 
+                  drop_ratio=drop_ratio,
+                  attn_drop_ratio=attn_drop_ratio,
+                  drop_path_ratio=dpr[i],
+                  norm_layer=norm_layer,
+                  act_layer=act_layer
+                  )
+            for i in range(depth)
+        ])
+        self.norm = norm_layer(embed_dim)
 
+        # representation layer
+        # 如果representation_size有值，且distilled为False
+        if representation_size and not distilled:
+            self.has_logits = True
+            self.num_features = representation_size
+            self.pre_logits = nn.Sequential(OrderedDict([
+                ("fc", nn.Linear(embed_dim, representation_size)),
+                ("act", nn.Tanh())
+            ]))
+        else:
+            self.has_logits = False
+            self.pre_logits = nn.Identity()
+        
+        # classifier head(s)
+        self.head = nn.Linear(self.num_features, num_classes) if num_classes > 0 else nn.Identity()
+        self.head_dist = None
+        if distilled:
+            self.head_dist = nn.Linear(self.embed_dim, self.num_classes) if num_classes > 0 else nn.Identity()
+
+        # weight init
+        nn.init.trunc_normal_(self.pos_embed, std=0.02)
+        if self.dist_token is not None:
+            nn.init.trunc_normal_(self.dist_token, std=0.02)
+        
+        nn.init.trunc_normal_(self.cls_token, std=0.02)
+        self.apply(_init_vit_weights)
+    
+    def forward_features(self, x):
+        # [B, C, H, W] -> [B, num_patches, embed_dim]
+        x = self.patch_embed(x)     # [B, 196, 768] if input_shape is 224 
+        
+        # [1, 1, 768] -> [B, 1, 768]
+        cls_token = self.cls_token.expand(x.shape[0], -1, -1)
+        if self.dist_token is None:
+            x = torch.cat((cls_token, x), 
+                          dim=1)    # [B, 196+1, 768]
+        else:
+            x = torch.cat((cls_token, self.dist_token.expand(x.shape[0], -1, -1), x), 
+                          dim=1)
+
+        x = self.pos_drop(x + self.pos_embed)
+        x = self.blocks(x)
+        x = self.norm(x)
+        if self.dist_token is None:
+            # distilled is True
+            # TODO: x[:, 0].shape = [4, 768]
+            # :表示第一个维度，即B, 0表示([197, 768])在197个768中选择索引为0的
+            # 即选择了第一个768
+            return self.pre_logits(x[:, 0])
+        else:
+            return x[:, 0], x[:, 1]
+    
+    def forward(self, x):
+        x = self.forward_features(x)
+
+        if self.head_dist is not None:
+            # distilled is True
+            # self.dist_token is nn.Parameter 并且 nn.Linear is nn.Linear
+            # 即x包含了两个值
+            x, x_dist = self.head(x[0]), self.head_dist(x[1])
+            if self.training and not torch.jit.is_scripting():
+                # 训练阶段, 并且是动态图模式
+                return x, x_dist
+            else:
+                # during inference, return the average of both classifier predictions
+                return (x + x_dist) / 2
+
+        else:
+            x = self.head(x)
+        
+        return x
+
+
+def _init_vit_weights(m):
+    """
+    ViT weight initialization
+    :param m: module
+    """
+    if isinstance(m, nn.Linear):
+        nn.init.trunc_normal_(m.weight, std=.01)
+        if m.bias is not None:
+            nn.init.zeros_(m.bias)
+    elif isinstance(m, nn.Conv2d):
+        nn.init.kaiming_normal_(m.weight, mode="fan_out")
+        if m.bias is not None:
+            nn.init.zeros_(m.bias)
+    elif isinstance(m, nn.LayerNorm):
+        nn.init.zeros_(m.bias)
+        nn.init.ones_(m.weight)
+
+
+def vit_base_patch16_224(num_classes: int=1000):
+    """
+    ViT-Base model (ViT-B/16) from original paper (https://arxiv.org/abs/2010.11929).
+    ImageNet-1k weights @ 224x224, source https://github.com/google-research/vision_transformer.
+    weights ported from official Google JAX impl:
+    链接: https://pan.baidu.com/s/1zqb08naP0RPqqfSXfkB2EA  密码: eu9f
+    """
+    model = VisionTransformer(img_size=224,
+                              patch_size=16,
+                              embed_dim=768,
+                              depth=12,
+                              num_heads=12,
+                              representation_size=None,
+                              num_classes=num_classes)
+    return model
+
+
+def vit_base_patch16_224_in21k(num_classes: int = 21843, has_logits: bool = True):
+    """
+    ViT-Base model (ViT-B/16) from original paper (https://arxiv.org/abs/2010.11929).
+    ImageNet-21k weights @ 224x224, source https://github.com/google-research/vision_transformer.
+    weights ported from official Google JAX impl:
+    https://github.com/rwightman/pytorch-image-models/releases/download/v0.1-vitjx/jx_vit_base_patch16_224_in21k-e5005f0a.pth
+    """
+    model = VisionTransformer(img_size=224,
+                              patch_size=16,
+                              embed_dim=768,
+                              depth=12,
+                              num_heads=12,
+                              representation_size=768 if has_logits else None,
+                              num_classes=num_classes)
+    return model
+
+
+def vit_base_patch32_224(num_classes: int = 1000):
+    """
+    ViT-Base model (ViT-B/32) from original paper (https://arxiv.org/abs/2010.11929).
+    ImageNet-1k weights @ 224x224, source https://github.com/google-research/vision_transformer.
+    weights ported from official Google JAX impl:
+    链接: https://pan.baidu.com/s/1hCv0U8pQomwAtHBYc4hmZg  密码: s5hl
+    """
+    model = VisionTransformer(img_size=224,
+                              patch_size=32,
+                              embed_dim=768,
+                              depth=12,
+                              num_heads=12,
+                              representation_size=None,
+                              num_classes=num_classes)
+    return model
+
+
+def vit_base_patch32_224_in21k(num_classes: int = 21843, has_logits: bool = True):
+    """
+    ViT-Base model (ViT-B/32) from original paper (https://arxiv.org/abs/2010.11929).
+    ImageNet-21k weights @ 224x224, source https://github.com/google-research/vision_transformer.
+    weights ported from official Google JAX impl:
+    https://github.com/rwightman/pytorch-image-models/releases/download/v0.1-vitjx/jx_vit_base_patch32_224_in21k-8db57226.pth
+    """
+    model = VisionTransformer(img_size=224,
+                              patch_size=32,
+                              embed_dim=768,
+                              depth=12,
+                              num_heads=12,
+                              representation_size=768 if has_logits else None,
+                              num_classes=num_classes)
+    return model
+
+
+def vit_large_patch16_224(num_classes: int = 1000):
+    """
+    ViT-Large model (ViT-L/16) from original paper (https://arxiv.org/abs/2010.11929).
+    ImageNet-1k weights @ 224x224, source https://github.com/google-research/vision_transformer.
+    weights ported from official Google JAX impl:
+    链接: https://pan.baidu.com/s/1cxBgZJJ6qUWPSBNcE4TdRQ  密码: qqt8
+    """
+    model = VisionTransformer(img_size=224,
+                              patch_size=16,
+                              embed_dim=1024,
+                              depth=24,
+                              num_heads=16,
+                              representation_size=None,
+                              num_classes=num_classes)
+    return model
+
+
+def vit_large_patch16_224_in21k(num_classes: int = 21843, has_logits: bool = True):
+    """
+    ViT-Large model (ViT-L/16) from original paper (https://arxiv.org/abs/2010.11929).
+    ImageNet-21k weights @ 224x224, source https://github.com/google-research/vision_transformer.
+    weights ported from official Google JAX impl:
+    https://github.com/rwightman/pytorch-image-models/releases/download/v0.1-vitjx/jx_vit_large_patch16_224_in21k-606da67d.pth
+    """
+    model = VisionTransformer(img_size=224,
+                              patch_size=16,
+                              embed_dim=1024,
+                              depth=24,
+                              num_heads=16,
+                              representation_size=1024 if has_logits else None,
+                              num_classes=num_classes)
+    return model
+
+
+def vit_large_patch32_224_in21k(num_classes: int = 21843, has_logits: bool = True):
+    """
+    ViT-Large model (ViT-L/32) from original paper (https://arxiv.org/abs/2010.11929).
+    ImageNet-21k weights @ 224x224, source https://github.com/google-research/vision_transformer.
+    weights ported from official Google JAX impl:
+    https://github.com/rwightman/pytorch-image-models/releases/download/v0.1-vitjx/jx_vit_large_patch32_224_in21k-9046d2e7.pth
+    """
+    model = VisionTransformer(img_size=224,
+                              patch_size=32,
+                              embed_dim=1024,
+                              depth=24,
+                              num_heads=16,
+                              representation_size=1024 if has_logits else None,
+                              num_classes=num_classes)
+    return model
+
+
+def vit_huge_patch14_224_in21k(num_classes: int = 21843, has_logits: bool = True):
+    """
+    ViT-Huge model (ViT-H/14) from original paper (https://arxiv.org/abs/2010.11929).
+    ImageNet-21k weights @ 224x224, source https://github.com/google-research/vision_transformer.
+    NOTE: converted weights not currently available, too large for github release hosting.
+    """
+    model = VisionTransformer(img_size=224,
+                              patch_size=14,
+                              embed_dim=1280,
+                              depth=32,
+                              num_heads=16,
+                              representation_size=1280 if has_logits else None,
+                              num_classes=num_classes)
+    return model
+    
 
 def test():
     i = torch.randn(4, 3, 224, 224)
     pe = PatchEmbed()
     o = pe(i)
+    print(o.shape)
 
-    l1 = nn.Linear(768, 768*3, bias=False)
     attn = Attention(dim=768, attn_drop_ratio=0.2, proj_drop_ratio=0.2)
     i1 = torch.randn(4, 196+1, 768)
     o1 = attn(i1)
     print(o1.shape)
 
+    dp = DropPath(drop_prob=0.4)
+    i2 = torch.randn(4, 196+1, 768)
+    o2 = dp(i2)
+    print(o2.shape)
 
-def test_kwargs(first, *args, **kwargs):
-    print('Required argument: ', first)
-    print(type(kwargs))
-    for v in args:
-        print('Optional argument (args): ', v)
-    for k, v in kwargs.items():
-        print(f'Optional argument {k} (kwargs): {v}')
+    block = Block(dim=768, num_heads=8, drop_ratio=0.2,
+                  attn_drop_ratio=0.2, drop_path_ratio=0.2)
+    i3 = torch.randn(4, 196+1, 768)
+    o3 = block(i3)
+    print(o3.shape)
+
+    cls_token = nn.Parameter(torch.zeros(1, 1, 768))
+    print("cls_token.shape = ", cls_token.shape)
+    cls_token = cls_token.expand(4, -1, -1)
+    print(cls_token.shape)
+
+    representation_size = 12
+    distilled = False
+    if representation_size and not distilled:
+        print("----")
+        x = torch.randn(4, 197, 768)
+        print(x.shape, x[:, 0].shape, x[:, 1].shape)
 
 
-def mod(n, m):
-    return n % m
+def main():
+    model = vit_base_patch16_224(num_classes=100)
+    i = torch.rand((4, 3, 224, 224))
+    o = model(i)
+    print(o.shape)
 
 
 if __name__ == "__main__":
-    test()
+    # test()
+    main()
