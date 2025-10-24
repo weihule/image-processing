f3a573b98dac1797adf6a7cec4b3029100f11319
weihule <weihule@outlook.com>
2025-10-24 18:10:40 +0800

    add some content

--- UNet-Pytorch/predict.py
+++ UNet-Pytorch/predict.py
@@ -1,6 +1,8 @@
 import os
 from PIL import Image
 from loguru import logger
+import cv2
+import matplotlib.pyplot as plt
 import numpy as np
 import torch
 import torch.nn.functional as F
@@ -8,7 +10,6 @@ from torchvision import transforms
 
 from utils.data_loading import BasicDataset
 from unet import UNet
-from utils.utils import plot_img_and_mask
 
 
 def preprocess(pil_img, scale):
@@ -38,11 +39,49 @@ def predict_img(net,
         output = net(img).cpu()
 
 
+def mask2image(mask: np.ndarray, mask_values):
+    if isinstance(mask_values[0], list):
+        out = np.zeros((mask.shape[-2], mask.shape[-1], len(mask_values[0])), dtype=np.uint8)
+
+
+def plot_img_and_mask(img, mask):
+    print(f"mask = {mask}")
+    classes = mask.max() + 1
+    fig, ax = plt.subplots(1, classes+1, figsize=(5 * (classes + 1), 5))
+    ax[0].set_title('Input image')
+    ax[0].imshow(img)
+    ax[0].axis('off')  # 隐藏坐标轴框
+    for i in range(classes):
+        ax[i + 1].set_title(f'Mask (class {i + 1})')
+        ax[i + 1].imshow(mask == i)
+        ax[i + 1].axis('off')
+    plt.xticks([]), plt.yticks([])
+    plt.show()
+
+
 def main():
     model_path = r""
-    classes = [0, 1]
+    classes = 2
     cfgs = {
         "model_path": model_path,
-        "classes": classes
+        "classes": classes,
+        "scale": 0.5,
+        "mask-threshold": 0.5,
+        "visualize": True,
+        "output_dir": ""
     }
+
+
+def test():
+    img_path = r'D:\workspace\data\images\carvana-image-masking-challenge\train\0cdf5b5d0ce1_01.jpg'
+    mask_path = r'D:\workspace\data\images\carvana-image-masking-challenge\train_masks\0cdf5b5d0ce1_01_mask.gif'
+    plot_img_and_mask(Image.open(img_path), np.array(Image.open(mask_path)))
+
+
+if __name__ == "__main__":
+    test()
+
+
+
+
     
--- UNet-Pytorch/unet/unet_model.py
+++ UNet-Pytorch/unet/unet_model.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 
-from unet_parts import DoubleDonv, Down, Up, OutConv
+from .unet_parts import DoubleDonv, Down, Up, OutConv
 
 
 class UNet(nn.Module):
